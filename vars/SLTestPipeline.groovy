169d471b2656ccce9f29a00448c5654d7ebcbb1a
Alejandro Bustelo <alejandro.bustelo@perficient.com>
2021-01-08 16:05:48 -0800

    test passing arguments

--- vars/SLTestPipeline.groovy
+++ vars/SLTestPipeline.groovy
@@ -11,6 +11,27 @@ def call(body) {
           }
         }
       }
+      stage("Stage Two") {
+        when {
+          body.enableFirsThing == 1
+        }
+        steps {
+          script {
+            sh 'echo This is stage 2'
+            sh "echo ${body.stringThing}"
+          }
+        }
+      }
+      stage("Stage Three") {
+        when {
+          body.enableSecondThing == 1
+        }
+        steps {
+          script {
+            sh 'This is stage 3'
+          }
+        }
+      }
     }
   }
 }
